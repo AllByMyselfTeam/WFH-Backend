7a121e16839cedc5e246d5fd5a2a761d2ed8c9d6
Cai <caicoding2@gmail.com>
2020-03-20 12:38:34 -0500

    add @service to taskService

--- src/main/java/com/app/service/TaskService.java
+++ src/main/java/com/app/service/TaskService.java
@@ -1,9 +1,14 @@
 package com.app.service;
 
+import java.util.List;
+
+import com.app.pojo.Checklist;
 import com.app.pojo.Task;
+
 
 public interface TaskService {
 	public Task getTaskById(int taskId);
 	public Task addTask(Task task);
 	public Task updateTask(Task task);
+	public List<Task> getAllTaskByCheck(Checklist check);
 }
--- src/main/java/com/app/serviceImpl/TaskServiceImpl.java
+++ src/main/java/com/app/serviceImpl/TaskServiceImpl.java
@@ -1,11 +1,16 @@
 package com.app.serviceImpl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.pojo.Checklist;
 import com.app.pojo.Task;
 import com.app.repository.TaskRepository;
 import com.app.service.TaskService;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 
 	@Autowired
@@ -23,7 +28,14 @@ public class TaskServiceImpl implements TaskService {
 
 	@Override
 	public Task updateTask(Task task) {
+		
 		return taskRepo.save(task);
+	}
+
+	@Override
+	public List<Task> getAllTaskByCheck(Checklist check) {
+		
+		return null;
 	}
 
 }
