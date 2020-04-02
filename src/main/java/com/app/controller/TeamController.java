0c1cfe5e6b1a82ac38c1d19a61cadf0e2dc5be9d
thitalow <thitacittalow@gmail.com>
2020-04-02 14:16:47 -0500

    team controller changes

---
+++ src/main/java/com/app/controller/TeamController.java
@@ -0,0 +1,29 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.pojo.Team;
+import com.app.service.TeamService;
+import com.app.service.UserService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class TeamController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private TeamService teamService;
+	
+	@PostMapping("/addTeam")
+	public Team login(@RequestBody Team team) {
+		System.out.println(team);
+		return teamService.addTeam(team);
+	}
+
+}
