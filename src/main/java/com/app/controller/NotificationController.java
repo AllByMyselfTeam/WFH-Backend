b7c4f56a45a275726c766f0863aa0ca162f07770
Cai <caicoding2@gmail.com>
2020-04-05 15:01:46 -0500

    Update notification Controller

--- src/main/java/com/app/controller/NotificationController.java
+++ src/main/java/com/app/controller/NotificationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,5 +31,10 @@ public class NotificationController {
 	@GetMapping("/team/{tid}")
 	public List<Notification> getAllNotification(@PathVariable("tid") int team){
 		return notificationService.getAllNotification(team);
+	}
+	
+	@DeleteMapping("/{notifyId}")
+	public void deleteNotification(@PathVariable("notifyId") int notifyId) {
+		notificationService.deleteNotification(notifyId);
 	}
 }
